23ab9b87ffdb6f42ff30299bfee44caf295b830d
Rageno2010 <83577853+Rageno2010@users.noreply.github.com>
2022-04-01 13:53:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var song1 = "";
+var song2 = "";
+
+function setup(){
+canvas = createCanvas(600,500);
+canvas.center();
+video = createCapture(VIDEO);
+video.hide();
+}
+function preload(){
+song1 = loadSound("music.mp3");
+song2 = loadSound("music2.mp3");
+}
+function draw(){
+image(video,0,0,600,500);
+}
+function play25(){
+    song.play();
+    
+}
